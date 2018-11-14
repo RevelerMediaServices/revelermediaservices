232cc126d941d808ab1c313f1fd7cddc92d45f6a
J. Graves <jwgravesfl@gmail.com>
2018-11-14 01:31:31 -0700

    register user route created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/posts/test
-// @desc    Test post route
-// @access  Public
+// @route   GET api/profile/test
+// @desc    Test profile route
+// @access  Private
 
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 
